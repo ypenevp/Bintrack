34c6d98abe29c37d0ab803faba0c782b7bb6a7c6
KrisMih <kristianmih15@gmail.com>
2026-03-06 19:29:06 +0200

    fix(backend): fix the backend so it can work with cross origins(this is needed for the work with the hardware part of the project).

--- backend/src/main/java/com/legends/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/legends/backend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 
                         .requestMatchers("/api/updates/getall/").permitAll() // View updates
                         .requestMatchers("/api/updates/getupdate/").permitAll()
-                        .requestMatchers("/api/v1/sensorData/**").permitAll()
+                        .requestMatchers("/api/v1/sensorData/add").permitAll()
 
                         .requestMatchers("/api/user/**").hasAnyRole("USER", "ADMIN", "WORKER")
 
--- backend/src/main/java/com/legends/backend/controllers/SensorDataController.java
+++ backend/src/main/java/com/legends/backend/controllers/SensorDataController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sensorData")
+@CrossOrigin(origins = "*")
 public class SensorDataController {
     private final SensorDataService sensorDataService;
     private final DeviceService deviceService;
