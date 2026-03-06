50a9c2a4540dbff04508a1adaaa80e71fc3d931d
KrisMih <kristianmih15@gmail.com>
2026-03-06 19:28:08 +0200

    fix(hardware): work with the new device system that was created in the previous commit.

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -118,6 +118,10 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <TinyGPSPlus.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "secrets.h"
 
 #define TRIG 12
 #define ECHO 11
@@ -134,9 +138,18 @@ HardwareSerial gpsSerial(2);
 TinyGPSPlus gps;
 
 float distance;
+float binLength = 20;
 unsigned long duration = 0;
+float fillPercentage;
+String status;
 
-void setup() {
+const char *ssid = NETWORK_NAME;
+const char *password = NETWORK_PASSWORD;
+const char *POSTserverURL = BACKEND_URL;
+const char *deviceToken = DEVICE_TOKEN;
+
+void setup() 
+{
   Serial.begin(115200);
   rgb.begin();
   rgb.show();
@@ -148,20 +161,43 @@ void setup() {
   
   digitalWrite(TRIG, LOW);
   delay(1000);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("Connected to WiFi!");
 }
 
-void lightIndication(float distance) {
+void lightIndication(float distance) 
+{
+
   if (distance > 20)
+  {
     rgb.setPixelColor(0, rgb.Color(255, 0, 0));
+  }
+
   else if (distance >= 10 && distance <= 20)
+  {
     rgb.setPixelColor(0, rgb.Color(255, 255, 0));
+  }
+
   else if (distance >= 0 && distance <= 10)
+  {
     rgb.setPixelColor(0, rgb.Color(0, 255, 0));
+  }
+
   rgb.show();
 }
 
-void loop() {
-  // 
+void loop() 
+{
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);
@@ -170,20 +206,26 @@ void loop() {
   
   duration = pulseIn(ECHO, HIGH);
   
-  if (duration >= 38000) {
+  if (duration >= 38000) 
+  {
     Serial.println("Out of range");
-  } else {
+  } 
+
+  else 
+  {
     distance = duration / 58.0;
     Serial.print(distance);
     Serial.println(" cm");
     lightIndication(distance);
   }
   
-  while (gpsSerial.available()) {
+  while (gpsSerial.available()) 
+  {
     gps.encode(gpsSerial.read());
   }
   
-  if (gps.location.isValid()) {
+  if (gps.location.isValid()) 
+  {
     Serial.print("Latitude: ");
     Serial.println(gps.location.lat(), 6);
     Serial.print("Longitude: ");
@@ -191,9 +233,58 @@ void loop() {
     Serial.print("Satellites: ");
     Serial.println(gps.satellites.value());
     Serial.println("----------------------");
-  } else {
+  } 
+
+  else 
+  {
     Serial.println("GPS: disconnected");
   }
-  
+
+  fillPercentage = (distance / binLength) * 100;
+
+  if(fillPercentage == 0)
+  {
+    status = "EMPTY";
+  }
+
+  else if(fillPercentage < 30)
+  {
+    status = "LOW";
+  }
+
+  else if(fillPercentage >= 30 && fillPercentage < 70)
+  {
+    status = "MEDIUM";
+  }
+
+  else if(fillPercentage >= 70)
+  {
+    status = "FULL";
+  }
+
+  if(WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+    String payload = "{";
+    payload += "\"deviceToken\": \"" + String(deviceToken) + "\",";
+    payload += "\"location\": \"42.123456,23.456789\",";
+    payload += "\"fillPercentage\": " + String(fillPercentage) + ",";
+    payload += "\"status\": \"" + status + "\"";
+    payload += "}";
+    http.begin(POSTserverURL);
+    http.addHeader("Content-Type", "application/json");
+    int responseCode = http.POST(payload);
+
+    if (responseCode > 0)
+    {
+      Serial.printf("Data sent at whole minute. HTTP %d\n", responseCode);
+    }
+    else
+    {
+      Serial.printf("HTTP error: %s\n", http.errorToString(responseCode).c_str());
+    }
+    http.end();
+  }
+
   delay(500);
 }
