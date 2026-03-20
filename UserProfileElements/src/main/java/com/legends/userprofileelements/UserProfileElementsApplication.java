e796f58b3098244ca3a0bab51a87ac89ec1258c3
DeyanAzSum <deyan662@gmail.com>
2026-03-20 18:49:21 +0200

    fix(backend): Fix user profile

---
+++ UserProfileElements/src/main/java/com/legends/userprofileelements/UserProfileElementsApplication.java
@@ -0,0 +1,13 @@
+package com.legends.userprofileelements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileElementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProfileElementsApplication.class, args);
+    }
+
+}
